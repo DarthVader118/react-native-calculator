c5acaed62f8a9b5dfb8c12208ebecef3ab4d4984
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-07-14 10:52:08 -0400

    Update EquationSolver.js

--- calculator/componenets/EquationSolver.js
+++ calculator/componenets/EquationSolver.js
@@ -1,2 +1,36 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react'
+
+const EquationSolver = () => {
+  handlePress = (type, value) => {
+
+  if(type === "clear") {
+ ;
+  }
+
+  if(type === "equal") {
+  }
+
+  if(operator === "+") {
+
+  }
+
+  if(operator === "/") {
+
+  }
+
+  if(operator === "-") {
+
+  }
+
+  if(operator === "*") {
+    }
+   }
+
+  }
+  render() {
+    return <Text>Hello, I am your cat!</Text>
+  }
+
+}
