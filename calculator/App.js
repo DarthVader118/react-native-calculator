c15d500d433270de9bf35ec62e27ae7b0c3c170d
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-14 10:47:29 -0400

    Update App.js

--- calculator/App.js
+++ calculator/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import EquationSolver from "./componenets/EquationSolver";
 
 
 export default function App() {
   return (
-    
+    <View style={styles.layout}>
+      <EquationSolver />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  layout : {
+    flex: 1,
+    backgroundColor: "black",
+  },
 });
