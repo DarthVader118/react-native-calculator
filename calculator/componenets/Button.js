52932b269a4f32056583c395f93a1fcb5f578c4c
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-15 11:01:52 -0400

    Update Button.js

--- calculator/componenets/Button.js
+++ calculator/componenets/Button.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { CalcButton } from "./Button"
 
-const Button = () => {
+export const ButtonContainer = props => {
+  return (
+    <View style={styles.buttonContainer}>
+      <CalcButton type="number" value="1" onPress={props.onPress}/>
+      <CalcButton type="number" value="2" onPress={props.onPress}/>
+      <CalcButton type="number" value="3" onPress={props.onPress}/>
+      <CalcButton type="number" value="4" onPress={props.onPress}/>
+      <CalcButton type="number" value="5" onPress={props.onPress}/>
+      <CalcButton type="number" value="6" onPress={props.onPress}/>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  buttonContainer : {
+    flex: 3,
+    flexDirection: "row",
+    
+  },
+});
+
