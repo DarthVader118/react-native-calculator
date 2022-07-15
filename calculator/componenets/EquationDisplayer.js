3aca94e4e6b3f14cc62e5c8b7500844280e63a39
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-15 10:33:49 -0400

    Update EquationDisplayer.js

--- calculator/componenets/EquationDisplayer.js
+++ calculator/componenets/EquationDisplayer.js
@@ -1,2 +1,26 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+
+export const EquationDisplayer = props => {
+  return (
+    <View style={styles.equationDisplayer}>
+      <Text style={styles.equationText}>{props.equation}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  equationDisplayer : {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    marginTop: 70,
+    marginBottom: 70,
+    marginHorizontal: 20,
+  },
+  equationText: {
+    fontSize: 70,
+    padding: 5,
+    color: "white",
+  },
+});
