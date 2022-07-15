4cb9c7ef88e6db376e736ca63e5844875bf01d2c
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-07-15 10:33:16 -0400

    Update ButtonContainer.js

--- calculator/componenets/ButtonContainer.js
+++ calculator/componenets/ButtonContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+
+export const ButtonContainer = () => {
+  return (
+    <View style={styles.buttonContainer}>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonContainer : {
+    flex: 3,
+  },
+});
