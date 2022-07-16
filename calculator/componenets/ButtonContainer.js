15b3bee7ebdfc03bdd975e5cdb1835df06c9e5eb
avishnubhatla <105984089+avishnubhatla@users.noreply.github.com>
2022-07-16 16:22:31 -0400

    Update ButtonContainer.js

--- calculator/componenets/ButtonContainer.js
+++ calculator/componenets/ButtonContainer.js
@@ -8,9 +8,19 @@ export const ButtonContainer = props => {
       <CalcButton type="number" value="1" onPress={props.onPress}/>
       <CalcButton type="number" value="2" onPress={props.onPress}/>
       <CalcButton type="number" value="3" onPress={props.onPress}/>
+      <CalcButton type="operator" value="/" onPress={props.onPress}/>
       <CalcButton type="number" value="4" onPress={props.onPress}/>
       <CalcButton type="number" value="5" onPress={props.onPress}/>
       <CalcButton type="number" value="6" onPress={props.onPress}/>
+      <CalcButton type="operator" value="*" onPress={props.onPress}/>
+      <CalcButton type="number" value="7" onPress={props.onPress}/>
+      <CalcButton type="number" value="8" onPress={props.onPress}/>
+      <CalcButton type="number" value="9" onPress={props.onPress}/>
+      <CalcButton type="operator" value="+" onPress={props.onPress}/>
+      <CalcButton type="clear" value='clear' onPress={props.onPress}/>
+      <CalcButton type="number" value="0" onPress={props.onPress}/>
+      <CalcButton type="equal" value='=' onPress={props.onPress}/>
+      <CalcButton type="operator" value="-" onPress={props.onPress}/>
     </View>
   )
 }
